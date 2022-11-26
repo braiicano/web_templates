7c72e308feda1499b73ef2ab1031eb6b3d8035fb
braianleonardocano@gmail.com <braianleonardocano@gmail.com>
2022-11-26 01:37:41 -0300

    Add more effects

--- show_expand/static/expand.js
+++ show_expand/static/expand.js
@@ -12,7 +12,21 @@ listItems.forEach((selectedItem) =>
       listItems.forEach(
         (_selectedItem) => (_selectedItem.style.display = "none")
       );
+      selectedItem.animate(
+        [{ width: "var(--width)" }, { width: "calc(var(--width) * 2)" }],
+        {
+          duration: 120,
+          interations: 1,
+        }
+      );
     } else {
+      selectedItem.animate(
+        [{ width: "calc(var(--width) *2)" }, { width: "var(--width)" }],
+        {
+          duration: 120,
+          interations: 1,
+        }
+      );
       listItems.forEach(
         (_selectedItem) => (_selectedItem.style.display = "initial")
       );
@@ -26,9 +40,10 @@ btns.forEach((selectedBtn) =>
     active = id.includes("prev") ? active - 1 : active + 1;
     if (active > lenListItems) active = 0;
     if (active < 0) active = lenListItems;
-    listItems.forEach((selectedItem) =>
-      selectedItem.classList.remove("--active")
-    );
+    listItems.forEach((selectedItem) => {
+      selectedItem.classList.remove("--active");
+    });
     listItems[active].classList.add("--active");
+    listItems[active].style.transition='.5s';
   })
 );
